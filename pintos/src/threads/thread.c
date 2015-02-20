7c27eecddd7714459bfea2948c24797bffc5b2a0
Emily Lutz <emily.lutz@berkeley.edu>
2015-02-20 02:41:08 +0000

    edited yield thread to put current thread back in ready queue according to priority

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -308,7 +308,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, list_priority_less_function, NULL); //changed by blackcats: used to be list_push_back(&ready_list, &cur->elem);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
